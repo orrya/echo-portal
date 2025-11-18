f4d2d45044a2d13086821104f804b3fccaeb2b52
Lee Calvert <lee.calvert@questemployment.co.uk>
2025-11-18 21:56:31 +0000

    fix build: install date-fns + clean next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-    serverActions: true
-  }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
