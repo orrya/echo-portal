4d25f4f0e5f46e90cd6ffa1691f6fecffa75c399
Lee Calvert <lee.calvert@orrya.co.uk>
2025-11-19 13:08:54 +0000

    change global css route

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import "../globals.css";
 import { Inter } from 'next/font/google'
 import NavBar from '@/components/NavBar'
 import { SupabaseProvider } from '@/components/SupabaseProvider'
