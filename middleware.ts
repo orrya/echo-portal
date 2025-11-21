e1a08b1f436b4d9495831d46055d5fe86667f341
Lee Calvert <lee.calvert@orrya.co.uk>
2025-11-21 21:01:10 +0000

    add / to middleware

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 const PUBLIC_PATH_PREFIXES = [
+  "/",
   "/auth/sign-in",
   "/auth/callback",
   "/auth/redirect", // <-- Added this path
