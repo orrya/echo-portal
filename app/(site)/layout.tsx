9490557b2a1804d695b521a814294e3982486d76
Lee Calvert <lee.calvert@orrya.co.uk>
2025-11-27 21:13:29 +0000

    Update app site layout to stop build error

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,4 +1,5 @@
-// app/(site)/layout.tsx
+export const dynamic = "force-dynamic";
+
 import type { ReactNode } from "react";
 import NavBar from "@/components/NavBar";
 
@@ -6,9 +7,7 @@ export default function SiteLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen flex flex-col">
       <NavBar />
-      <div className="flex-1 pt-4 pb-10">
-        {children}
-      </div>
+      <div className="flex-1 pt-4 pb-10">{children}</div>
     </div>
   );
 }
